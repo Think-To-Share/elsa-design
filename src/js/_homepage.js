9cb9b10e47b380000dba1d424471a0554bee570b
Rahul Dey <rahul900day@gmail.com>
2021-09-06 18:13:38 +0530

    some changes

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -5,5 +5,9 @@ if(document.querySelector('.homepage .testimonial-section')) {
         container: '.homepage .testimonial-section .testimonial-slider',
         items: 3,
         autoplay: true,
+        autoplayButtonOutput: false,
+        nav: false,
+        controlsPosition: 'bottom',
+        controlsText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
     })
 }
